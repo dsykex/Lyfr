0ae2718f34468f3ac9bb46e939d8bc3e6f834ee5
dsykex <dezmensykes.ds@gmail.com>
2019-06-06 11:31:16 -0500

    Created login pages and other changes

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {AlertController} from '@ionic/angular';
+
 import * as firebase from 'firebase';
 
 @Component({
@@ -6,22 +9,104 @@ import * as firebase from 'firebase';
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
+
 export class LoginPage implements OnInit
 {
   public user: any = {};
-  constructor() 
+  public errorMsg: any = '';
+
+  constructor(public router: Router, public alertController: AlertController) 
   { 
 
   }
 
   ngOnInit()
   {
-
+    
   }
 
   login()
   {
+    if(this.user.email && this.user.password)
+    {
+      firebase.auth().signInWithEmailAndPassword(this.user.email, this.user.password).then(()=>{
+        this.router.navigateByUrl('/home');
+      }).catch(() => {
+        this.errorMsg = 'An error occured. Make sure your login credentials are correct and your network is established.';
+        setTimeout(() => {
+          this.errorMsg = '';
+        }, 3000);
+      });
+    }
+  }
+
+  signup()
+  {
 
   }
 
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Prompt!',
+      inputs: [
+        {
+          name: 'name1',
+          type: 'text',
+          placeholder: 'Placeholder 1'
+        },
+        {
+          name: 'name2',
+          type: 'text',
+          id: 'name2-id',
+          value: 'hello',
+          placeholder: 'Placeholder 2'
+        },
+        {
+          name: 'name3',
+          value: 'http://ionicframework.com',
+          type: 'url',
+          placeholder: 'Favorite site ever'
+        },
+        // input date with min & max
+        {
+          name: 'name4',
+          type: 'date',
+          min: '2017-03-01',
+          max: '2018-01-12'
+        },
+        // input date without min nor max
+        {
+          name: 'name5',
+          type: 'date'
+        },
+        {
+          name: 'name6',
+          type: 'number',
+          min: -5,
+          max: 10
+        },
+        {
+          name: 'name7',
+          type: 'number'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Ok',
+          handler: (data) => {
+            console.log(data);
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 }
